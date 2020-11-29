ac86b5e607d8de342aeea3085aff4d14c836f998
Daniel Gleison <72145423+danielgleison@users.noreply.github.com>
2020-11-28 21:29:26 -0300

    Update Raspberry-MQTT-Server.py

--- Raspberry-MQTT-Server.py
+++ Raspberry-MQTT-Server.py
@@ -12,19 +12,9 @@ client = mqtt.Client()
 
 data = []
 
-
-# These functions handle what happens when the MQTT client connects
-
-# to the broker, and what happens then the topic receives a message
-
 def on_connect(client, userdata, flags, rc):
 
-    # rc is the error code returned when connecting to the broker
-
     print ("Connected!")
-
-
-    # Once the client has connected to the broker, subscribe to the topic
 
     client.subscribe(mqtt_topic)
 
@@ -32,17 +22,7 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
 
-    # This function is called everytime the topic is published to.
-
-    # If you want to check each message, and do something depending on
-
-    # the content, the code to do this should be run in this function
-
-   
-    # print ("Topic: ", msg.topic + "\nMessage: " + str(msg.payload))
-    # print ("Message: " + str(msg.payload))
-    # client.publish(mqtt_topic,msg.payload,qos=0)
-    
+  
     now = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     temp = msg.payload.decode('utf-8')
     
@@ -51,31 +31,12 @@ def on_message(client, userdata, msg):
     df.to_csv('dataset_IoT.csv', mode = 'a', header = False)
     print(df)
   
-
-    # The message itself is stored in the msg variable
-
-    # and details about who sent it are stored in userdata
- 
-
-# Here, we are telling the client which functions are to be run
-
-# on connecting, and on receiving a message
-
 client.on_connect = on_connect
 
 client.on_message = on_message
 
- 
-
-# Once everything has been set up, we can (finally) connect to the broker
-
-# 1883 is the listener port that the MQTT broker is using
-
 client.connect(mqtt_broker_ip, mqtt_broker_port)
  
-
-# Once we have told the client to connect, let the client object run itself
-
 client.loop_forever()
 
 client.disconnect()
